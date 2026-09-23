965a45e43541e6b774fe87f9909ce91dbd1d06de
arunkrana5 <135095303+arunkrana5@users.noreply.github.com>
2026-09-23 23:53:59 +0530

    Set Shopkeeper POS PWA as primary root web app and move Admin Panel to /admin

--- src/VillageShop.Api/Program.cs
+++ src/VillageShop.Api/Program.cs
@@ -251,7 +251,7 @@ IResult ServeApkFile(IWebHostEnvironment env)
 }
 
 app.MapControllers();
-app.MapFallbackToFile("pos/{*path}", "pos/index.html");
+app.MapFallbackToFile("admin/{*path}", "admin/index.html");
 app.MapFallbackToFile("index.html");
 
 app.Run();
